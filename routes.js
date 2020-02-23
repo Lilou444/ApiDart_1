7d0bfbf9f7872ac4df56d76a94525350a8f61e54
lilou <leilaphebidias@gmail.com>
2020-02-23 14:58:12 +0100

    file routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,38 @@
 const router = require('express').Router()
 
-router.get('/', function (req, res, next) {
-  // get
+ 
+const Game = requirer('./routers/Game.js')
+const Player = requirer('./routers/Player.js')
+
+// error à ajouter non encore traitée 
+
+
+// routes 
+router.use('/games',Game)
+router.use('/players', Player)
+
+
+router.get('/', function (err,req, res, next) {
+  res.format({
+      html: function () {
+          res.redirect('/games')
+      },
+      json: function () {
+          res.status(err.status)
+      }
+  })
 })
 
 
-router.get('/', function (req, res, next) {
-   //get
+router.get('/', function (err, req, res, next) {
+    res.format({
+        html: function () {
+            res.render(err)
+        },
+        json: function ()  {
+            res.status(err.status)
+        }
+    })
 })
 
 module.exports = router
