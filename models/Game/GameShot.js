81044e3652de637000ad83df83c1ab9271dd01f9
lilou <leilaphebidias@gmail.com>
2020-02-23 01:40:19 +0100

    base model tp redis-mongo
    these are the functions found in my tp redis-mongo rendered

--- models/Game/GameShot.js
+++ models/Game/GameShot.js
@@ -29,6 +29,75 @@ const GameShot = new Schema({
 
 const modelGameShot = mongoose.model('GameShot', GameShot)
 
-module.exports = modelGameShot
+module.exports = {
+
+    // fonction get 
+
+  get: async (userId) => {
+      console.log("get user")
+      const user = await User.find({id:userId})
+      console.log(user)
+      return user
+    },
+  ​
+    count: async() => {
+     return await User.count({})
+    },
+  ​
+
+  // fonction getAll
+    getAll: async (limit, offset) => {
+      const users = await User.find()
+    //  console.log(users)
+     return users
+  ​
+    },
+  ​
+    // fonction insert 
+    insert: async (params) => {
+      const userId = require('uuid').v4()
+      let user = { id: userId,
+                  pseudo: params.pseudo,
+                  firstname: params.firstname,
+                  lastname: params.lastname,
+                  email: params.email,
+                  password: params.password }
+      let doc = await User.collection.insertMany([user], function(err) {
+            console.log("insert error")
+        })
+      console.log(user)
+      console.log(doc)
+  ​
+      return doc
+  ​
+    },
+  ​
+   // fonction update 
+    update: async(userId, params) => {
+      let user = { id: userId,
+        pseudo: params.pseudo,
+        firstname: params.firstname,
+        lastname: params.lastname,
+        email: params.email,
+        password: params.password }
+      const doc = await User.updateOne({id:userId},user, function(err) {
+          console.log("update error")
+      })
+  ​
+      return doc
+   
+    },
+  ​
+   // fonction delete
+    remove: async(userId) => {
+      console.log("delete")
+      const doc = await User.deleteOne({id:userId})
+      return doc
+    }
+   
+}
+
 
 //Models route gameShot
+
+
